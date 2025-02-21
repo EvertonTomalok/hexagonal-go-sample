5a1c363216223710da5d3848c93a037e01e7c53d
Everton Tomalok <evertontomalok123@gmail.com>
2025-02-20 22:20:14 -0300

    memDB repo

---
+++ internal/repositories/memDB.go
@@ -0,0 +1,58 @@
+package repositories
+
+import (
+	"errors"
+
+	"github.com/EvertonTomalok/ports-challenge/internal/domain"
+)
+
+var MaxSizeAchieveErr = errors.New("max size achieved")
+
+type Option func(*config)
+
+func WithMaxSize(len int) Option {
+	return func(c *config) {
+		c.maxLen = len
+	}
+}
+
+type config struct {
+	maxLen int
+}
+
+func NewMemDB(options ...Option) *memDB {
+	config := &config{maxLen: 1048576} // default max len is 2 ** 20
+	for _, o := range options {
+		o(config)
+	}
+
+	db := &memDB{data: make(map[string]domain.Port), maxSize: config.maxLen}
+
+	return db
+}
+
+// In memory map struct database
+type memDB struct {
+	data    map[string]domain.Port
+	maxSize int
+}
+
+// Upsert the database ports collection.
+func (repo *memDB) Upsert(key string, value domain.Port) error {
+	// if value is not present on map but max size was achieve, it will
+	// return error
+	if len(repo.data) == repo.maxSize {
+		_, found := repo.Get(key)
+		if !found {
+			return MaxSizeAchieveErr
+		}
+	}
+
+	repo.data[key] = value
+	return nil
+}
+
+func (repo *memDB) Get(key string) (domain.Port, bool) {
+	item, found := repo.data[key]
+	return item, found
+}
---
+++ internal/repositories/memDD_test.go
@@ -0,0 +1,87 @@
+package repositories
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/EvertonTomalok/ports-challenge/internal/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func parsePort(jsonRaw string) (domain.Port, error) {
+	var portData domain.Port
+	err := json.Unmarshal([]byte(jsonRaw), &portData)
+
+	return portData, err
+}
+
+func Test_MemDB(t *testing.T) {
+	key := "AEAJM"
+	portRaw := `{"name":"Ajman","city":"Ajman","country":"United Arab Emirates","alias":[],"regions":[],"coordinates":[55.5136433,25.4052165],"province":"Ajman","timezone":"Asia/Dubai","unlocs":["AEAJM"],"code":"52000"}`
+	portData, err := parsePort(portRaw)
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	memDB := NewMemDB()
+	t.Run("insert port", func(t *testing.T) {
+		err := memDB.Upsert(key, portData)
+		assert.Nil(t, err)
+
+		value, found := memDB.Get(key)
+		assert.True(t, found)
+		assert.Equal(t, "Ajman", value.Name)
+	})
+
+	t.Run("update port", func(t *testing.T) {
+		portData.Name = "Testing" // overwritten name
+
+		err := memDB.Upsert(key, portData)
+		assert.Nil(t, err)
+
+		value, found := memDB.Get(key)
+		assert.True(t, found)
+		assert.Equal(t, "Testing", value.Name)
+	})
+}
+
+func Test_MemDB_Limits(t *testing.T) {
+	memDB := NewMemDB(WithMaxSize(1)) // set map to have max size 1
+
+	type testCase struct {
+		testName string
+		portRaw  string
+		key      string
+		mustFail bool
+	}
+	testCases := []testCase{
+		{
+			testName: "first insert must work",
+			portRaw:  `{"name":"Ajman","city":"Ajman","country":"United Arab Emirates","alias":[],"regions":[],"coordinates":[55.5136433,25.4052165],"province":"Ajman","timezone":"Asia/Dubai","unlocs":["AEAJM"],"code":"52000"}`,
+			key:      "AEAJM",
+			mustFail: false,
+		},
+		{
+			testName: "second insert must fail",
+			portRaw:  `{"name": "Abu Dhabi", "coordinates": [54.37, 24.47], "city": "Abu Dhabi", "province": "Abu Z¸aby [Abu Dhabi]", "country": "United Arab Emirates", "alias": [], "regions": [], "timezone": "Asia/Dubai", "unlocs": ["AEAUH"], "code": "52001"}`,
+			key:      "AEAUH",
+			mustFail: true,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.testName, func(t *testing.T) {
+			portData, err := parsePort(test.portRaw)
+			if err != nil {
+				t.Fail()
+				return
+			}
+			err = memDB.Upsert(test.key, portData)
+			assert.Equal(t, test.mustFail, err != nil)
+			if test.mustFail {
+				assert.ErrorIs(t, err, MaxSizeAchieveErr)
+			}
+		})
+	}
+}
